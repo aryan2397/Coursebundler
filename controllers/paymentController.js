88da2c0e47d3fc3860185230363dde0aa379daa0
aryan2397 <135629240+aryan2397@users.noreply.github.com>
2024-03-17 00:36:12 +0530

    Bug Fixed

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -27,7 +27,7 @@ export const buySubscription = catchAsyncError(async (req, res, next) => {
 
     res.status(201).json({
         success: true,
-        subscription
+        subscriptionId: subscription.id,
     })
 
 })

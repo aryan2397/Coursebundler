dff2b9ab11e8733a715caa1c8b2eee1904d177aa
aryan2397 <135629240+aryan2397@users.noreply.github.com>
2024-03-17 00:10:05 +0530

    Bug Fixed

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -41,8 +41,7 @@ export const paymentVerification = catchAsyncError(async (req, res, next) => {
 
     const subscription_id = user.subscription.id;
 
-    const secretKey = process.env.RAZORPAY_API_SECRET || 'fallback_secret_key';
-    const generated_signature = crypto.createHmac("sha256", secretKey)
+    const generated_signature = crypto.createHmac("sha256", process.env.RAZORPAY_API_SECRET)
         .update(razorpay_payment_id + "|" + subscription_id, "utf-8").digest("hex");
 
 

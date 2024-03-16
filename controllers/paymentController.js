677ae3135d76c3ffad62c4cce80d8021b5f90d1b
aryan2397 <135629240+aryan2397@users.noreply.github.com>
2024-03-17 01:01:10 +0530

    Bug Fixed

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -9,26 +9,27 @@ export const buySubscription = catchAsyncError(async (req, res, next) => {
 
     const user = await User.findById(req.user._id);
 
-    if (user.role === "admin") return next(new ErrorHandler("Admin can't buy subscription ", 404))
+    if (user.role === "admin")
+        return next(new ErrorHandler("Admin can't buy subscription", 400));
 
-    const plan_id = process.env.PLAN_ID;
+    const plan_id = process.env.PLAN_ID || "plan_NkN5pjSkLNTL9m";
 
     const subscription = await instance.subscriptions.create({
-        plan_id: plan_id,
+        plan_id,
         customer_notify: 1,
         total_count: 12,
     });
 
-
     user.subscription.id = subscription.id;
+
     user.subscription.status = subscription.status;
 
     await user.save();
 
     res.status(201).json({
         success: true,
-        subscription,
-    })
+        subscriptionId: subscription.id,
+    });
 
 })
 
